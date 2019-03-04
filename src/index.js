4c02baf2b579df7dfbd87b305c26d5bd90689bfd
soia <nikita.soya@gmail.com>
2019-03-04 22:53:08 +0200

    add styles

--- src/index.js
+++ src/index.js
@@ -1,21 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import AppHeader from './components/app-header';
-import SearchPanel from './components/search-panel';
-import TodoList from './components/todo-list';
 
-const App = () => {
-    const isLoggedIn = false;
-    const LoginBox = <span>Log in please</span>
+import App from './components/app/app';
 
-    return (
-        <div>
-            {isLoggedIn ? null : LoginBox}
-            <AppHeader />
-            <SearchPanel />
-            <TodoList />
-        </div>
-    );
-};
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render( < App /> ,
+  document.getElementById('root'));
